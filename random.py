bed800ece3db7ea3cfa2e142fa7783af234c3b80
Sushama Jori <46436054+sushmajori@users.noreply.github.com>
2020-08-26 23:30:04 +0530

    Create random.py

---
+++ random.py
@@ -0,0 +1,39 @@
+x=1
+y=1.1
+z='111'
+x=int(1)
+y=int(1.1)
+z=int('111')
+print(x,y,z)
+
+w='''
+
+Hii everyone
+my name is XYZ.I am from Pune .
+i completed my ssc in jay malhar high school jambut at my home town.
+after that i completed my diploma in samarth polytechnic belhe in computer department.
+and right now i am pursuing BTech from MIT academy of engineering aalandi in IT department.
+'''
+
+print('MY JOURNEY IS'+w)
+
+a='hello world'
+print(a[1])
+print(a[1:-1])
+print(a[2:-5])
+print(a[2:5])
+
+a=" Btech XYzMIT  "
+print(a.strip())
+
+b="hello world"
+print(b.upper())
+print(b.lower())
+print(len(b))
+print(b.replace('h','S'))
+
+age=20
+name='I am Xyz  and I am {} '
+print(name.format(age))
+txt="hello"
+x=txt[0]
